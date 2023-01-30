81d0544325376eb95c04cba07ef62b54837c5334
Albert <a.mejia@gmx.net>
2023-01-30 16:00:03 +0100

    update commit

--- question.php
+++ question.php
@@ -26,10 +26,41 @@
     <?php echo $question["question_text"];?>
     <!-- END: PHP INCLUDE -->
 
-    <!-- Page Content Start -->
-    
 
-    <!-- Page Content End -->
+    <!-- START: PAGE CONTENT -->
+    <form id="quiz-form" action="question.php" method="post">
+    <?php
+    // Generiere Antwort-Radio-Buttons mit Beschriftung
+
+    // Single-Choice: Hole den Namen der richtigen Antwortspalte in $correct, aus $question["$correct"]
+    $correct = $question["correct"];
+
+    for ($a= 1; $a <= 5; $a++) {
+    // Setze für $answerColumnName den Namen der Tabellenspalte "answer-N" zusammen
+    $answerColumnName = "answer-" . $a;
+
+    // Falls überhaupt Antworttext in $question[$answerColumnName] gibt
+    // und der Antworttext nicht gleich '', dann...
+    if (isset($question[$answerColumnName]) && $question[$answerColumnName] !== '') {
+        // hole den Antworttext aus $question
+        $answerText = $question[$answerColumnName];
+
+        // Entscheide für $value, wieviele Punkte die Antwort ergibt:
+        // richtig -> 1 Punkt, falsch = 0 Punkte
+        if ($correct === $answerColumnName) $value = 1;
+        else $value = 0;
+
+        echo "<div class='form-check'>
+        <input class='form-check-input' type='radio' name='single-choice' id='$answerColumnName' value='$value'>
+        <label class='form-check-label' for='$answerColumnName'>$answerText</label>
+        </div>";
+    }
+    }
+    
+    ?>
+    <input type="submit" value="Submit">
+    </form>
+    <!-- END: Page Content -->
 
     <!--BOOTSTRAP and LOCAL Javascript-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
--- report.php
+++ report.php
@@ -12,34 +12,14 @@
 
 </head>
 <body>
-    <!-- Page Content Start -->
-    <form id="quiz-form" action="question.php" method="post" onsubmit="return navigate('next');">
-    <?php
-    // Generiere Antwort-Radio-Buttons mit Beschriftung
+    <!-- START: PHP INCLUDE -->
+    <?php require "./includes/db.php"; ?>
+    <!-- END: PHP INCLUDE -->
 
-    // Single-Choice: Hole den Namen der richtigen Antwortspalte in $correct, aus $question["$correct"]
-    $correct = $question["correct"];
 
-    for ($a= 1; $a <= 5; $a++) {
-    // Setze für $answerColumnName den Namen der Tabellenspalte "answer-N" zusammen
-    $answerColumnName = "answer-" . $a;
-
-    // Falls überhaupt Antworttext in $question[$answerColumnName] gibt
-    // und der Antworttext nicht gleich '', dann...
-    if (isset($question[$answerColumnName]) && $question[$answerColumnName] !== '') {
-        // hole den Antworttext aus $question
-        $answerText = $question[$answerColumnName];
-
-        // Entscheide für $value, wieviele Punkte die Antwort ergibt:
-        // richtig -> 1 Punkt, falsch = 0 Punkte
-        if ($correct === $answerColumnName) $value = 1;
-        else $value = 0;
-    }
-
-    }
+    <!-- START: Page Content -->
     
-    ?>
-    <!-- Page Content End -->
+    <!-- END: Page Content -->
 
     <!--BOOTSTRAP and LOCAL Javascript-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
